5331d50bf853e97481bdebdd50314397d776a805
Nick Lopez <116652056+FenrirIsFree@users.noreply.github.com>
2024-06-24 13:48:25 -0400

    Get all href from links in header on homepage

--- spelling_scraper.py
+++ spelling_scraper.py
@@ -4,9 +4,21 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 base_url = 'https://www.dannyboyspizza.com/'
+header_links = []
 page_text = []
 
 result = requests.get(base_url)
 result_html = BeautifulSoup(result.text, 'html.parser')
 
-print(result_html)
+# Find all the links in header of the homepage
+header = result_html.header
+links = header.find_all('li')
+
+for link in links:
+    header_links.append({
+        'anchor name': link.find('a').get_text(strip=True),
+        'anchor href': link.find('a')['href']
+    })
+
+
+print(header_links)
