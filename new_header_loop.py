7ffe3f3eb1fdcf737a0716c121d043bb46e58a08
Nick Lopez <116652056+FenrirIsFree@users.noreply.github.com>
2024-06-27 12:07:20 -0400

    Logic to check if href contains URL

--- new_header_loop.py
+++ new_header_loop.py
@@ -8,26 +8,41 @@ url = 'index'
 
 # Find all the links in header of the homepage
 header_links = []
+header_urls = []
+
+result = requests.get(f'{base_url}{url}')
+result_html = BeautifulSoup(result.text, 'html.parser')
+header = result_html.header
+links = header.find_all('li')
+
+for link in links:
+    header_links.append({
+        'anchor name': link.find('a').get_text(strip=True),
+        'anchor href': link.find('a')['href']
+    })
+
 counter = 1
 
 while url:
-    result = requests.get(f'{base_url}{url}')
-    print(f'scraping {base_url}{url}...')
-    result_html = BeautifulSoup(result.text, 'html.parser')
-    header = result_html.header
-    links = header.find_all('li')
+    if 'https://' not in url and 'http://' not in url:
+        result = requests.get(f'{base_url}{url}')
+        print(f'scraping {base_url}{url}...')
+        result_html = BeautifulSoup(result.text, 'html.parser')
+    else:
+        if url not in header_urls:
+            result = requests.get(f'{url}')
+            print(f'scraping {url}...')
+            result_html = BeautifulSoup(result.text, 'html.parser')
+            
+        header_urls.append(url)
 
-    for link in links:
-        header_links.append({
-            'anchor name': link.find('a').get_text(strip=True),
-            'anchor href': link.find('a')['href']
-        })
-
-    url = header_links[counter]['anchor href'] if counter < len(header_links) else None
+    if counter < len(header_links):
+        url = header_links[counter]['anchor href']
+    else: 
+        url = None
     counter += 1
     sleep(2)
 
-print(header_links)
 # # Maybe easier way to grab data?
 # # Initialize a dictionary to hold all headers
 # headers = {}
